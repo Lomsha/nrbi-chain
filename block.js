c75f1d92d6d2f1ee1910888e291c56f85582796f
Benshalom <benshalom.kariuki@kopokopo.com>
2022-05-30 12:57:46 +0300

    Built the SHA256 Hash function

--- block.js
+++ block.js
@@ -1,3 +1,6 @@
+const SHA256 = require('crypto-js/sha256');
+
+
 class Block {
     constructor(timestamp, lastHash, hash, data) {
         this.timestamp = timestamp;
@@ -19,9 +22,13 @@ class Block {
       static mineBlock(lastBlock, data) {
         const timestamp = Date.now();
         const lastHash = lastBlock.hash;
-      const hash = 'todo-hash';
+      const hash = Block.hash(timestamp, lastHash, data);
       return new this(timestamp, lastHash, hash, data);
      }
+
+     static hash(timestamp, lastHash, data) {
+      return SHA256(`${timestamp}${lastHash}${data}`).toString();
+    }
     }
 
     module.exports = Block;
