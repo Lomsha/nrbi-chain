3685b51d451f65d48bbf55515c4683520d823e0f
Benshalom <benshalom.kariuki@kopokopo.com>
2022-06-02 08:58:44 +0300

    Create blockchain network: finished setting up and testing the connections over p2p server

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -11,7 +11,19 @@ class P2pServer {
   listen() {
     const server = new Websocket.Server({ port: P2P_PORT });
     server.on('connection', socket => this.connectSocket(socket));
+    this.connectToPeers();
+    console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
   }
+
+
+
+  connectToPeers() {
+    peers.forEach(peer => {
+      const socket = new Websocket(peer);
+      socket.on('open', () => this.connectSocket(socket));
+    });
+  }
+
 
   connectSocket(socket) {
     this.sockets.push(socket);
