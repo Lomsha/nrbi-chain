62db1b4d0f595573bb49a8f9a751864ae05e5b0f
Lomsha <benshalom.kariuki@kopokopo.com>
2022-06-07 09:11:07 +0300

    Creating the blockchain network: handling messages from peers

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -28,6 +28,15 @@ class P2pServer {
   connectSocket(socket) {
     this.sockets.push(socket);
     console.log('Socket connected');
+    this.messageHandler(socket);
+    socket.send(JSON.stringify(this.blockchain.chain));
+  }
+
+  messageHandler(socket) {
+    socket.on('message', message => {
+      const data = JSON.parse(message);
+      console.log('data', data);
+    });
   }
 }
 
