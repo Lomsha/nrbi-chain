2ca06d643c91ff53299810acacba8e5ffc6e47b0
Lomsha <benshalom.kariuki@kopokopo.com>
2022-06-10 06:42:35 +0300

    added synchronisation and replacement feature to the blockchain

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -29,13 +29,28 @@ class P2pServer {
     this.sockets.push(socket);
     console.log('Socket connected');
     this.messageHandler(socket);
-    socket.send(JSON.stringify(this.blockchain.chain));
+    this.sendChain(socket);
   }
 
   messageHandler(socket) {
     socket.on('message', message => {
       const data = JSON.parse(message);
-      console.log('data', data);
+
+
+      this.blockchain.replaceChain(data);
+    });
+  }
+
+  
+  sendChain(socket) {
+    socket.send(JSON.stringify(this.blockchain.chain));
+  }
+
+
+
+  syncChains() {
+    this.sockets.forEach(socket => {
+      this.sockets.forEach(socket => this.sendChain(socket));
     });
   }
 }
