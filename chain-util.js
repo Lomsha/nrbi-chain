ef6278b9ce01cf08c3fa7cef4d53c39d8d624faa
Lomsha <benshalom.kariuki@kopokopo.com>
2022-07-12 01:13:06 +0300

    Tested the transaction function

--- chain-util.js
+++ chain-util.js
@@ -1,5 +1,5 @@
 const EC = require('elliptic').ec;
-const uuidV1 = require('uuid/v1');
+const uuidV1 = require('uuid');
 const ec = new EC('secp256k1');
 
 class ChainUtil {
@@ -7,7 +7,7 @@ class ChainUtil {
         return ec.genKeyPair();
    }
    static id() {
-    return uuidV1();
+    return uuidV1.v1();
   }
 }
 
