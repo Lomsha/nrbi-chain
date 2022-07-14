41fa2db37ed097b9b89aa78f13d2dd6a8840b854
Lomsha <benshalom.kariuki@kopokopo.com>
2022-07-14 21:38:04 +0300

    added a test to make sure that the input object for a transaction when they are signed are created

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -21,6 +21,11 @@ describe('Transaction', () => {
         expect(transaction.outputs.find(output => output.address === recipient).amount)
           .toEqual(amount);
       });
+
+    it('inputs the balance of the wallet', () => {
+        expect(transaction.input.amount).toEqual(wallet.balance);
+     });
+    
       describe('transacting with an amount that exceeds the balance', () => {
         beforeEach(() => {
           amount = 50000;
