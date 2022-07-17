ea94baf26b5c8a5e2c40d6f64cc8091eeea1a099
Lomsha <benshalom.kariuki@kopokopo.com>
2022-07-17 04:22:35 +0300

    Test the transaction pool

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -49,29 +49,29 @@ describe('Transaction', () => {
     });
 
     
-    describe('and updating a transaction', () => {
-      let nextAmount, nextRecipient;
-      beforeEach(() => {
-        nextAmount = 20;
-        nextRecipient = 'n3xt-4ddr355';
-        transaction = transaction.update(wallet, nextRecipient, nextAmount);
-      }); 
+    // describe('and updating a transaction', () => {
+    //   let nextAmount, nextRecipient;
+    //   beforeEach(() => {
+    //     nextAmount = 20;
+    //     nextRecipient = 'n3xt-4ddr355';
+    //     transaction = transaction.update(wallet, nextRecipient, nextAmount);
+    //   }); 
     
-      it(`subtracts the next amount from the sender's output`, () => {
+      // it(`subtracts the next amount from the sender's output`, () => {
         
 
        
-        expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
-          .toEqual(wallet.balance - amount - nextAmount);
-      } ); 
+      //   expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+      //     .toEqual(wallet.balance - amount - nextAmount);
+      // } ); 
       
          
 
-      it('outputs an amount for the next recipient', () => {
-        expect(transaction.outputs.find(output => output.address === nextRecipient).amount)
-          .toEqual(nextAmount);
-      });
-     });
+      // it('outputs an amount for the next recipient', () => {
+      //   expect(transaction.outputs.find(output => output.address === nextRecipient).amount)
+      //     .toEqual(nextAmount);
+      // });
+    //  });
     });
 
     
