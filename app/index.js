e52ce4c28686eab3206ce9afddf883ecf1bcf693
Lomsha <benshalom.kariuki@kopokopo.com>
2022-07-18 10:33:18 +0300

    Added get transactions end point

--- app/index.js
+++ app/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
-
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const p2pServer = new P2pServer(bc);
 
 
@@ -25,6 +28,10 @@ app.post('/mine', (req, res) => {
     res.redirect('/blocks');
   });
   
+
+  app.get('/transactions', (req, res) => {
+    res.json(tp.transactions);
+  });
 app.listen(HTTP_PORT, () => console.log(`Listening on port: ${HTTP_PORT}`));
 
 p2pServer.listen();
