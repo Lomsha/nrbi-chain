c4c2cca35ea7e2d2658f25dbf30340cb158c333d
Lomsha <benshalom.kariuki@kopokopo.com>
2022-07-18 06:18:58 +0300

    Created Transactions with the wallet

--- wallet/transaction-pool.js
+++ wallet/transaction-pool.js
@@ -11,6 +11,9 @@ class TransactionPool {
         this.transactions.push(transaction);
       }
     }
+    existingTransaction(address) {
+      return this.transactions.find(transaction => transaction.input.address === address);
+    }
   }
   
   module.exports = TransactionPool;
