2196be9853fe44f94731f0603d3d5885af1cd518
Lomsha <benshalom.kariuki@kopokopo.com>
2022-07-19 11:40:01 +0300

    added public key endpoint

--- app/index.js
+++ app/index.js
@@ -39,6 +39,10 @@ app.post('/mine', (req, res) => {
     p2pServer.broadcastTransaction(transaction);
     res.redirect('/transactions');
   });
+
+  app.get('/public-key', (req, res) => {
+    res.json({ publicKey: wallet.publicKey })
+  })
   
 app.listen(HTTP_PORT, () => console.log(`Listening on port: ${HTTP_PORT}`));
 
