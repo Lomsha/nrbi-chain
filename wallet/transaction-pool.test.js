afb16c79cf2a61532b10edb9bd67cd5e449aa0f7
Lomsha <benshalom.kariuki@kopokopo.com>
2022-07-21 08:16:38 +0300

    Tested the valid transaction method

--- wallet/transaction-pool.test.js
+++ wallet/transaction-pool.test.js
@@ -8,8 +8,6 @@ describe('TransactionPool', () => {
     tp = new TransactionPool();
     wallet = new Wallet();
     transaction = Transaction.newTransaction(wallet, 'r4nd-4dr355', 30);
-    tp.updateOrAddTransaction(transaction);
-    // transaction = wallet.createTransaction('r4nd-4dr355', 30, bc, tp);
   });
 
   it('adds a transaction to the pool', () => {
@@ -22,5 +20,28 @@ describe('TransactionPool', () => {
     tp.updateOrAddTransaction(newTransaction);
     expect(JSON.stringify(tp.transactions.find(t => t.id === newTransaction.id)))
       .not.toEqual(oldTransaction);
+  });
+  describe('mixing valid and corrupt transactions', () => {
+    let validTransactions;
+    beforeEach(() => {
+      validTransactions = [...tp.transactions];
+      for (let i=0; i<6; i++) {
+        wallet = new Wallet();
+        transaction = wallet.createTransaction('r4nd-4dr355', 30, tp);
+        if (i%2==0) {
+          transaction.input.amount = 9999;
+        } else {
+          validTransactions.push(transaction);
+        }
+      }
+    });
+  
+    it('shows a difference between valid and corrupt transactions', () => {
+      expect(JSON.stringify(tp.transactions)).not.toEqual(JSON.stringify(validTransactions));
+    });
+  
+    it('grabs valid transactions', () => {
+      expect(tp.validTransactions()).toEqual(validTransactions);
+    });
   });
 });
