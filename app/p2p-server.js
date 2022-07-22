788bebcdf0f528dd2004284ae98c6c4bab42d8c5
Lomsha <benshalom.kariuki@kopokopo.com>
2022-07-22 08:20:51 +0300

    Added broadcast clear transaction

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -3,7 +3,8 @@ const P2P_PORT = process.env.P2P_PORT || 5001;
 const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 const MESSAGE_TYPES = {
   chain: 'CHAIN',
-  transaction: 'TRANSACTION'
+  transaction: 'TRANSACTION',
+  clear_transactions: 'CLEAR_TRANSACTIONS'
 };
 
 class P2pServer {
@@ -47,7 +48,9 @@ class P2pServer {
       case MESSAGE_TYPES.transaction:
         this.transactionPool.updateOrAddTransaction(data.transaction);
         break;  
-
+      case MESSAGE_TYPES.clear_transactions:
+        this.transactionPool.clear();
+        break;
       }
 
     });
@@ -79,6 +82,13 @@ class P2pServer {
       this.sockets.forEach(socket => this.sendTransaction(socket, transaction));
   });
 }
+
+broadcastClearTransactions() {
+  this.sockets.forEach(socket => socket.send(JSON.stringify({
+    type: MESSAGE_TYPES.clear_transactions
+  })));
+}
+
 }
 
 module.exports = P2pServer;
