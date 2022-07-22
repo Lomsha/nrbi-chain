06d50c9e36a9e302af2f58f4c28bbab848bbab4d
Lomsha <benshalom.kariuki@kopokopo.com>
2022-07-22 07:31:20 +0300

    Tested the reward transaction function

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -1,6 +1,7 @@
 const Transaction = require('./transaction');
 const Wallet = require('./index');
 const { newTransaction } = require('./transaction');
+const { MINING_REWARD } = require('../config');
 
 describe('Transaction', () => {
     let transaction, wallet, recipient, amount;
@@ -49,29 +50,40 @@ describe('Transaction', () => {
     });
 
     
-    // describe('and updating a transaction', () => {
-    //   let nextAmount, nextRecipient;
-    //   beforeEach(() => {
-    //     nextAmount = 20;
-    //     nextRecipient = 'n3xt-4ddr355';
-    //     transaction = transaction.update(wallet, nextRecipient, nextAmount);
-    //   }); 
+    describe('and updating a transaction', () => {
+      let nextAmount, nextRecipient;
+      beforeEach(() => {
+        nextAmount = 20;
+        nextRecipient = 'n3xt-4ddr355';
+        transaction = transaction.update(wallet, nextRecipient, nextAmount);
+      }); 
     
-      // it(`subtracts the next amount from the sender's output`, () => {
+      it(`subtracts the next amount from the sender's output`, () => {
         
 
        
-      //   expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
-      //     .toEqual(wallet.balance - amount - nextAmount);
-      // } ); 
+        expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount)
+          .toEqual(wallet.balance - amount - nextAmount);
+      } ); 
       
          
 
-      // it('outputs an amount for the next recipient', () => {
-      //   expect(transaction.outputs.find(output => output.address === nextRecipient).amount)
-      //     .toEqual(nextAmount);
-      // });
-    //  });
-    });
+      it('outputs an amount for the next recipient', () => {
+        expect(transaction.outputs.find(output => output.address === nextRecipient).amount)
+          .toEqual(nextAmount);
+      });
+     });
 
+
+describe('creating a reward transaction', () => {
+  beforeEach(() => {
+    transaction = Transaction.rewardTransaction(wallet, Wallet.blockchainWallet());
+  });
+
+  it(`rewards the miner's wallet`, () => {
+    expect(transaction.outputs.find(output.address === wallet.publicKey).amount)
+    .toEqual(MINING_REWARD);
+     });
+    });
+  });
     
