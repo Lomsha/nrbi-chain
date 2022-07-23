456ac31e1172564e54e26558cc34de7cca70518a
Lomsha <benshalom.kariuki@kopokopo.com>
2022-07-23 16:38:09 +0300

    Added the calculate wallet balance method

--- wallet/index.js
+++ wallet/index.js
@@ -38,6 +38,41 @@ class Wallet {
     }
 
 
+
+    calculateBalance(blockchain) {
+      let balance = this.balance;
+      let transactions = [];
+      blockchain.chain.forEach(block => block.data.forEach(transaction => {
+        transaction.push(transaction);
+      }));
+
+      const walletInputTs = transactions
+      .filter(transaction => transactions.input.address === this.publicKey);
+
+      let startTime =0;
+
+      if (walletInputTs.length > 0) {
+      const recentInputT = walletInputTs.reduce(
+        (prev, current) => prev.input.timestamp > current.input.timestamp ? prev : current
+      );
+
+      balance = recentInputT.outputs.find(output => output.address === this.publicKey).amount;
+      startTime = recentInputT.input.timestamp;
+     } 
+     transactions.forEach(transaction => {
+         if (transactions.input.timestamp > startTime)  {
+            transaction.outputs.find(output => {
+              if (output.address === this.publicKey) {
+                balance += output.amount
+              }
+            })
+         }
+     });
+
+     return balance;
+
+    }
+
     static blockchainWallet() {
       const blockchainWallet = new this();
       blockchainWallet.address = 'blockchain-wallet';
